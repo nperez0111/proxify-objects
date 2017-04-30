6f84bb9825fff660d1bc4f3c4cf1c29f84217fba
Nick The Sick <nperez0111@gmail.com>
2017-04-30 09:34:59 -0700

    got tests working

--- test.js
+++ test.js
@@ -1,54 +1,54 @@
-import test from 'ava';
-import fn from './';
+const test = require( 'ava' ),
+    fn = require( './' );
 const pojo = { radius: 1, area: Math.PI },
-    circle = fn(pojo, {
-        area(obj) {
+    circle = fn( pojo, {
+        area( obj ) {
             return obj.radius * obj.radius * Math.PI
         }
     }, {
-        area(obj, property, value) {
-            obj.radius = Math.sqrt(value / Math.PI)
+        area( obj, property, value ) {
+            obj.radius = Math.sqrt( value / Math.PI )
             return value
         }
-    }),
+    } ),
     a = r => r * r * Math.PI
-test('Test original size', t => {
-    t.deepEqual(pojo.radius, 1)
-    t.deepEqual(pojo.area, Math.PI)
-})
+test( 'Test original size', t => {
+    t.deepEqual( pojo.radius, 1 )
+    t.deepEqual( pojo.area, Math.PI )
+} )
 
-test('Get Proxy values', t => {
-    t.deepEqual(circle.radius, 1)
-    t.deepEqual(circle.area, Math.PI)
-})
+test( 'Get Proxy values', t => {
+    t.deepEqual( circle.radius, 1 )
+    t.deepEqual( circle.area, Math.PI )
+} )
 
-test('Set Proxy values', t => {
-    for (let i = 0; i < 12; i++) {
+test( 'Set Proxy values', t => {
+    for ( let i = 0; i < 12; i++ ) {
         circle.radius = i
-        t.deepEqual(circle.area, a(i))
-        t.deepEqual(circle.radius, i)
+        t.deepEqual( circle.area, a( i ) )
+        t.deepEqual( circle.radius, i )
     }
-})
+} )
 
-test('Set Proxied Setter', t => {
-    for (let i = 0; i < 12; i++) {
+test( 'Set Proxied Setter', t => {
+    for ( let i = 0; i < 12; i++ ) {
         circle.area = i * Math.PI * i
-        t.deepEqual(circle.area, a(i))
-        t.deepEqual(circle.radius, i)
+        t.deepEqual( circle.area, a( i ) )
+        t.deepEqual( circle.radius, i )
     }
-})
+} )
 
-test('Get Target', t => {
-    circle.area = a(9)
-    t.deepEqual(pojo.radius, 9)
-    t.deepEqual(pojo.area, a(9))
+test( 'Get Target', t => {
+    circle.area = a( 9 )
+    t.deepEqual( pojo.radius, 9 )
+    t.deepEqual( pojo.area, a( 9 ) )
 
     circle.radius = 10
-    t.deepEqual(pojo.radius, 10)
-    t.deepEqual(pojo.area, a(9))
+    t.deepEqual( pojo.radius, 10 )
+    t.deepEqual( pojo.area, a( 9 ) )
         //desired as we are not attempting to keep the pojo in sync
 
-})
+} )
 
 
 const tri = {
@@ -69,69 +69,69 @@ const tri = {
     base: 1
 }
 
-const triangle = fn(tri, {
-    area(obj) {
+const triangle = fn( tri, {
+    area( obj ) {
         return obj.height * obj.base / 2
     },
-    height(obj) {
-        const get = (xOrY) => ['a', 'b', 'c'].map(prop => {
-                return obj[prop][xOrY]
-            }),
-            min = get('y').reduce((a, b) => {
-                return Math.min(a, b)
-            }),
-            max = get('y').reduce((a, b) => {
-                return Math.max(a, b)
-            })
+    height( obj ) {
+        const get = ( xOrY ) => [ 'a', 'b', 'c' ].map( prop => {
+                return obj[ prop ][ xOrY ]
+            } ),
+            min = get( 'y' ).reduce( ( a, b ) => {
+                return Math.min( a, b )
+            } ),
+            max = get( 'y' ).reduce( ( a, b ) => {
+                return Math.max( a, b )
+            } )
         return max - min
     },
-    base(obj) {
-        const get = (xOrY) => ['a', 'b', 'c'].map(prop => {
-                return obj[prop][xOrY]
-            }),
-            min = get('x').reduce((a, b) => {
-                return Math.min(a, b)
-            }),
-            max = get('x').reduce((a, b) => {
-                return Math.max(a, b)
-            })
+    base( obj ) {
+        const get = ( xOrY ) => [ 'a', 'b', 'c' ].map( prop => {
+                return obj[ prop ][ xOrY ]
+            } ),
+            min = get( 'x' ).reduce( ( a, b ) => {
+                return Math.min( a, b )
+            } ),
+            max = get( 'x' ).reduce( ( a, b ) => {
+                return Math.max( a, b )
+            } )
         return max - min
     }
-})
+} )
 
-test('triangle', t => {
-    t.deepEqual(triangle.area, 1)
-    t.deepEqual(triangle.height, 2)
-    t.deepEqual(triangle.base, 1)
-})
+test( 'triangle', t => {
+    t.deepEqual( triangle.area, 1 )
+    t.deepEqual( triangle.height, 2 )
+    t.deepEqual( triangle.base, 1 )
+} )
 
-test('set triangle prop', t => {
+test( 'set triangle prop', t => {
     triangle.b = { x: 0, y: 4 }
     triangle.c = { x: 4, y: 0 }
-    t.deepEqual(triangle.area, 8)
-    t.deepEqual(triangle.height, 4)
-    t.deepEqual(triangle.base, 4)
-})
+    t.deepEqual( triangle.area, 8 )
+    t.deepEqual( triangle.height, 4 )
+    t.deepEqual( triangle.base, 4 )
+} )
 
 
-const reverse = fn({}, {}, {
-    defaultCase(obj, property, value, originalObj) {
-        originalObj[value] = property
+const reverse = fn( {}, {}, {
+    defaultCase( obj, property, value, originalObj ) {
+        originalObj[ value ] = property
     }
-})
+} )
 
-test('default case of setting - tests reverse', t => {
+test( 'default case of setting - tests reverse', t => {
     reverse.value = 'key'
-    t.deepEqual(reverse.key, 'value')
-})
+    t.deepEqual( reverse.key, 'value' )
+} )
 
-const returnsKey = fn({ alreadyHere: 12 }, {
-    defaultCase(obj, property, orginalObj) {
+const returnsKey = fn( { alreadyHere: 12 }, {
+    defaultCase( obj, property, orginalObj ) {
         return property
     }
-}, {})
+}, {} )
 
-test('default case of getting - tests returns key that isnt on object already', t => {
-    t.deepEqual(returnsKey.value, 'value')
-    t.deepEqual(returnsKey.alreadyHere, 12)
-})
+test( 'default case of getting - tests returns key that isnt on object already', t => {
+    t.deepEqual( returnsKey.value, 'value' )
+    t.deepEqual( returnsKey.alreadyHere, 12 )
+} )
